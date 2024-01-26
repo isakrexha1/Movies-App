a1cffbe1df32f41094a64b0a225091c82ad9f574
Isak Rexha <rexhaisak@gmail.com>
2024-01-26 01:02:51 +0100

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import IndividualMovie from './movies/IndividualMovie';
+import { landingPageDTO } from './movies/movies.model';
+import MoviesList from './movies/MoviesList';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [movies, setMovies] = useState <landingPageDTO>({});
+
+    useEffect(() => {
+      const timerId = setTimeout(() => {
+        setMovies({
+          inTheaters: [
+          {
+            id:1,
+            title: "Taken",
+            poster: 'https://lumiere-a.akamaihd.net/v1/images/image_efeee89d.jpeg?region=0%2C0%2C800%2C1200'
+          },
+        
+          {
+            id:2,
+            title: "Avengers: Age of Ultron",
+            poster: 'https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg'
+          }
+        ],
+
+        upcomingReleases: [
+          {
+            id: 3,
+            title: 'Family Plan',
+            poster: 'https://m.media-amazon.com/images/M/MV5BNGY3ZGNmZGMtNzBhNi00Y2U5LWI3ZTgtMjM4NDk3MjA1MDQ5XkEyXkFqcGdeQXVyNjE2MzI5ODM@._V1_.jpg'
+          }
+        ]
+        })
+      }, 200);
+      
+      return () => clearTimeout(timerId);
+    });
+
+
+  return(
+
+    <div className="container">
+      <h3>In Theaters</h3>
+      <MoviesList movies={movies.inTheaters}/>
+
+      <h3>Upcoming Releases</h3>
+      <MoviesList movies={movies.upcomingReleases}/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/utils/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return(
+        <img src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=6c09b952ah189kwcjkv2czl7iug1a7q2ejb0zcso5nkk67t1&ep=v1_gifs_search&rid=200w.gif&ct=g" alt="Loading" />
+    )
+}
