4be883e4c520f6ac6f4b931c9719561e1cf2f7a1
Isak Rexha <rexhaisak@gmail.com>
2024-02-11 15:34:47 +0100

    .

--- MoviesAPI/Filters/MyActionFilter.cs
+++ MoviesAPI/Filters/MyActionFilter.cs
@@ -1,6 +1,24 @@
-﻿namespace MoviesAPI.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace MoviesAPI.Filters
 {
-    public class MyActionFilter
+    public class MyActionFilter : IActionFilter
     {
+        private readonly ILogger<MyActionFilter> logger;
+        public  MyActionFilter(ILogger<MyActionFilter> logger)
+        {
+            this.logger = logger;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            logger.LogWarning("OnActionExecuting");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            logger.LogWarning("OnActionExecuted");
+        }
     }
 }
