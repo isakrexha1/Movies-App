163d23d3c30151af7ea3ebaa633d1470d1c181e9
kaltrina-morina <km51097@ubt-uni.net>
2024-02-27 16:15:31 +0100

    Index Movie Theaters

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -2,3 +2,4 @@ const baseURL = process.env.REACT_APP_API_URL;
 
 export const urlGenres = `${baseURL}/genres`;
 export const urlActors = `${baseURL}/actors`;
+export const urlMovieTheaters = `${baseURL}/movietheaters`;
--- src/movietheaters/CreateMovieTheater.tsx
+++ src/movietheaters/CreateMovieTheater.tsx
@@ -1,12 +1,33 @@
+import axios from "axios";
+import { urlMovieTheaters } from "../endpoints";
 import MovieTheaterForm from "./MovieTheaterForm";
+import { movieTheaterCreationDTO } from "./movieTheater.model";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import DisplayErrors from "../utils/DisplayErrors";
 
 export default function CreateMovieTheater() {
+  const history = useHistory();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  async function create(movieTheater: movieTheaterCreationDTO) {
+    try {
+      await axios.post(urlMovieTheaters, movieTheater);
+      history.push("/movietheaters");
+    } catch (error) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
+  }
+
   return (
     <>
       <h3>Create Movie Theater</h3>
+      <DisplayErrors errors={errors} />
       <MovieTheaterForm
         model={{ name: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={async (values) => await create(values)}
       />
     </>
   );
